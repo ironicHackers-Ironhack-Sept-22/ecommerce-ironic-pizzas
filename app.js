b4ee68a76312575ea4a623194e03c985b777223a
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-09-28 11:17:44 +0200

    hbs views: create pages for 3 pizzas & setup hbs

--- app.js
+++ app.js
@@ -4,19 +4,61 @@ const app = express();
 
 app.use(express.static('public')); // Make everything inside of public/ available
 
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
 
 
-app.get("/", (request, response, next) => {
+app.get("/", (req, res, next) => {
     console.log("this is the homepage");    
-    // response.send("hello world");
-    response.sendFile(__dirname + '/views/index.html');
+    // res.send("hello world");
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
-app.get("/contact", (request, response, next) => {
+app.get("/contact", (req, res, next) => {
     console.log("this is the contact page");
-    response.sendFile(__dirname + '/views/contact-page.html');
+    res.sendFile(__dirname + '/views/contact-page.html');
 });
+
+
+app.get("/pizzas/margherita", (req, res, next) => {
+    //res.send("hello")
+    //res.sendFile(__dirname + '/views/pizza-margherita.html');
+
+    const data = {
+        title: "Pizza Margherita",
+        price: 8,
+        imgFile: "pizza-margherita.jpg"
+    }
+
+    res.render("pizza-page", data);
+});
+
+
+app.get("/pizzas/carbonara", (req, res, next) => {
+
+    const data = {
+        title: "Pizza Carbonara",
+        price: 10,
+        imgFile: "pizza-carbonara.jpg"
+    }
+
+    res.render("pizza-page", data);
+});
+
+
+app.get("/pizzas/funghi", (req, res, next) => {
+
+    const data = {
+        title: "Pizza Funghi",
+        price: 10,
+        imgFile: "pizza-funghi.jpg"
+    }
+
+    res.render("pizza-page", data);
+});
+
+
 
 
 
