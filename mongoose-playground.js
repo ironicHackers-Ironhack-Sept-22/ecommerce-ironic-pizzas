851b985e493bc4920e77f4bb40e55520cb1fb6f8
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-09-30 10:53:53 +0200

    refactor: extract Pizza model to specific file

--- mongoose-playground.js
+++ mongoose-playground.js
@@ -1,32 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-
-// schema = pattern that all documents in a collection will follow.
-
-
-const pizzaSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    }, 
-    price: {
-        type: Number,
-        required: true,
-    },
-    isVeggie: {
-        type: Boolean,
-        default: false
-    },
-    ingredients: [String],
-    dough: {
-        type: String,
-        enum: ["thin", "thick", "with cheese", "with garlic"]
-    }
-});
-
-const Pizza = mongoose.model('Pizza', pizzaSchema);
+const Pizza = require("./models/Pizza.model");
 
 
 mongoose
